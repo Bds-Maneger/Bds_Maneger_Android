5f6a84b13592feef07e312952738c94110b43432
Matheus sampaio Queiroga <srherobrine20@gmail.com>
2021-03-21 18:54:13 -0300

    push

--- get_build.sh
+++ get_build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cordova platform add android
 cordova build
-mkdir outputs
-mv -fv platforms/android/app/build/outputs/apk/*/*.apk outputs/
+mkdir outputs || rm -fv outputs/*
+# mv -fv platforms/android/app/build/outputs/apk/*/*.apk outputs/
+zipalign -v -p 4 platforms/android/app/build/outputs/apk/*/*.apk outputs/signed.apk
+apksigner sign --ks Android_Key.keystore --out outputs/assigned.apk outputs/signed.apk
--- index.js
+++
@@ -1,4 +0,0 @@
-console.log("loged")
-module.exports = {
-    dirname : path.join(__dirname, 'template_src')
-};
